bf78402a37fa9b20bf416676737a75c3cdefaa67
maxiwell <maxiwell@gmail.com>
2016-07-29 09:46:46 -0300

    Adding 'ldconfig' in install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cp ./lib/* /usr/lib/x86_64-linux-gnu/
 ln -frs /usr/lib/x86_64-linux-gnu/libavcodec.so.54.71.100   /usr/lib/x86_64-linux-gnu/libavcodec.so.54
 ln -frs /usr/lib/x86_64-linux-gnu/libavformat.so.54.36.100 /usr/lib/x86_64-linux-gnu/libavformat.so.54
 ln -frs /usr/lib/x86_64-linux-gnu/libavutil.so.52.6.100    /usr/lib/x86_64-linux-gnu/libavutil.so.52
-
+ldconfig
 
 
